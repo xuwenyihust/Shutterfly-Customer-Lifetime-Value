795dcb6d0bdb20fdf510c2a7ebf11ec1fa753c1c
xuwenyihust <1016635786@qq.com>
2017-06-18 16:14:40 -0700

    Add timeframe

--- src/clv.py
+++ src/clv.py
@@ -1,6 +1,7 @@
 import os
 import json
 import argparse
+from datetime import datetime
 
 
 class customer_info(object):
@@ -8,19 +9,11 @@ class customer_info(object):
 	def __init__(self, customer_id):
 		# Customer ID
 		self.customer_id = customer_id
-		# Sum of customer expenditures
-		#self.sum_expenditures = 0
 		# Record all the orders
 		# HashMap<order_id, amount>
 		self.orders = dict()
 		# Number of site visits
 		self.num_visits = 0
-		# Record all the visits
-		#self.visits = []
-		# Time of fisrt event of this customer
-		self.start_time = None
-		# Time of last event of this customer
-		self.end_time = None
 
 	# The function that ingest event to the corresponding customer
 	def ingest(self, event):
@@ -30,11 +23,72 @@ class customer_info(object):
 		# Append the expenditures
 		elif event['type'] == 'ORDER':
 			if event['verb'] == 'NEW':
-				self.orders[event['key']] = event['total_amount']
+				#self.orders[event['key']] = event['total_amount']
+				self.orders[event['key']] = self.get_amount(event['total_amount'])
 			elif event['verb'] == 'UPDATE':
-				self.orders[event['key']] = event['total_amount']
+				#self.orders[event['key']] = event['total_amount']
+				self.orders[event['key']] = self.get_amount(event['total_amount'])
 			# Inappropriate field content
 			#else:
+
+	# Extract money value
+	def get_amount(self, total_amount):
+		money_value = total_amount.split()[0]
+		try:
+			money_value = float(money_value)
+			return money_value
+		except:
+			return 0
+
+class time_frame(object):
+
+	def __init__(self):
+		self.start_time = None
+		self.end_time = None
+
+	def ingest(self, event):
+		# Parse the time field
+		event_time = event['event_time']
+		event_date = self.parse_event_time(event_time)
+		
+		# Update the earlist time
+		self.start_time = self.get_start_time(event_date)
+		# Update the latest time
+		self.end_time = self.get_end_time(event_date)
+
+	# Parse the time field
+	# %Y-%m-%d
+	def parse_event_time(self, event_time):
+		# Only need the year-month-date part
+		return event_time.split('T')[0]
+
+	# Select the earlist event time
+	def get_start_time(self, event_date):
+		# If self.start_time is None
+		if not self.start_time:
+			return event_date
+
+		date0 = datetime.strptime(self.start_time, '%Y-%m-%d')
+		date1 = datetime.strptime(event_date, '%Y-%m-%d')
+
+		if date0 <= date1:
+			return self.start_time
+		else:
+			return event_date
+
+	# Select the latest event time
+	def get_end_time(self, event_date):
+		# If self.end_time is None
+		if not self.end_time:
+			return event_date
+
+		date0 = datetime.strptime(self.end_time, '%Y-%m-%d')
+		date1 = datetime.strptime(event_date, '%Y-%m-%d')
+		
+		if date0 <= date1:
+			return event_date
+		else:
+			return self.end_time
 
 
 def main():
--- tests/clv_test.py
+++ tests/clv_test.py
@@ -1,12 +1,82 @@
 import pytest
 from src.clv import customer_info
-
-def test_ingest_new_customer():
-	ci = customer_info('96f55c7d8f42')
-
-	assert True
+from src.clv import time_frame
+from datetime import datetime
 
 
+def test_ci_get_amount():
+	ci = customer_info('0')
+	assert ci.get_amount("12.34 USD") == 12.34
+	assert ci.get_amount("12.34 XXX") == 12.34
+	assert ci.get_amount("12.3u4 XXX") == 0
 
+
+def test_ci_ingest():
+	ci = customer_info('1')
+	event0 = {"type": "SITE_VISIT", "verb": "NEW", "key": "ac05e815502f", "event_time": "2017-01-06T12:45:52.041Z", "customer_id": "96f55c7d8f42", "tags": [{"some key": "some value"}]}
+	ci.ingest(event0)
+	assert ci.num_visits == 1
+	event1 = {"type": "ORDER", "verb": "NEW", "key": "68d84e5d1a43", "event_time": "2017-01-06T12:55:55.555Z", "customer_id": "96f55c7d8f42", "total_amount": "12.34 USD"}
+	ci.ingest(event1)
+	assert ci.orders['68d84e5d1a43'] == 12.34
+
+
+def test_ci_ingest_visit():
+	ci = customer_info('1')
+	event0 = {"type": "SITE_VISIT", "verb": "NEW", "key": "ac05e815502f", "event_time": "2017-01-06T12:45:52.041Z", "customer_id": "96f55c7d8f42", "tags": [{"some key": "some value"}]}
+	event1 = {"type": "SITE_VISIT", "verb": "NEW", "key": "ac05e815502g", "event_time": "2017-01-06T12:45:52.041Z", "customer_id": "96f55c7d8f42", "tags": [{"some key": "some value"}]}
+	event2 = {"type": "SITE_VISIT", "verb": "NEW", "key": "ac05e815502h", "event_time": "2017-01-06T12:45:52.041Z", "customer_id": "96f55c7d8f42", "tags": [{"some key": "some value"}]}
+	ci.ingest(event0)
+	ci.ingest(event1)
+	ci.ingest(event2)
+	assert ci.num_visits == 3
+
+
+def test_ci_ingest_order():
+	ci = customer_info('1')
+	event0 = {"type": "ORDER", "verb": "NEW", "key": "68d84e5d1a43", "event_time": "2017-01-06T12:55:55.555Z", "customer_id": "96f55c7d8f42", "total_amount": "12.34 USD"}
+	event1 = {"type": "ORDER", "verb": "NEW", "key": "68d84e5d1a44", "event_time": "2017-01-06T12:55:55.555Z", "customer_id": "96f55c7d8f42", "total_amount": "12.34 USD"}
+	event2 = {"type": "ORDER", "verb": "NEW", "key": "68d84e5d1a45", "event_time": "2017-01-06T12:55:55.555Z", "customer_id": "96f55c7d8f42", "total_amount": "12.34 USD"}
+	ci.ingest(event0)
+	ci.ingest(event1)
+	ci.ingest(event2)
+	assert len(ci.orders) == 3
+	assert sum(ci.orders.values()) == 3*12.34
+
+
+def test_tf_parse_event_time():
+	tf = time_frame()
+	assert tf.parse_event_time('2017-01-06T12:46:46.384Z') == '2017-01-06'
+
+
+def test_tf_get_start_time():
+	tf = time_frame()
+	tf.start_time = '2017-01-06'
+	assert tf.get_start_time('2017-01-03') == '2017-01-03'
+	assert tf.get_start_time('2017-07-03') == '2017-01-06'
+	assert tf.get_start_time('2017-01-06') == '2017-01-06'
+	assert tf.get_start_time('1999-11-20') == '1999-11-20'
+	
+	tf.start_time = None
+	assert tf.get_start_time('2017-01-03') == '2017-01-03'
+
+
+def test_tf_get_end_time():
+	tf = time_frame()
+	tf.end_time = '2017-12-10'
+	assert tf.get_end_time('2017-01-03') == '2017-12-10'
+	assert tf.get_end_time('2000-01-03') == '2017-12-10'
+	assert tf.get_end_time('2050-10-01') == '2050-10-01'
+
+	tf.end_time = None
+	assert tf.get_end_time('2017-01-03') == '2017-01-03'
+
+
+def test_tf_ingest():
+	tf = time_frame()
+	event = {"type": "SITE_VISIT", "verb": "NEW", "key": "ac05e815502f", "event_time": "2017-01-06T12:45:52.041Z", "customer_id": "96f55c7d8f42", "tags": [{"some key": "some value"}]}
+	tf.ingest(event)
+	assert tf.start_time == '2017-01-06'
+	assert tf.end_time == '2017-01-06'
 
 
